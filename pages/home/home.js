bc91206bc279840464a81e4f833824d648fa808b
Xuhaojie <531807259@qq.com>
2020-02-10 13:50:37 +0800

    小程序的初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
-// pages/home/home.js
+//将变量联动到显示内容中
 Page({
+data:{
+  name: 'Coderwhy',
+  age: 18,
+  students: [
+    { id: 110, name: 'kobe', age: 30 },
+    { id: 111, name: 'james', age: 28 },
+    { id: 112, name: 'rose', age: 31 },
+    { id: 113, name: 'why', age: 18 }
+  ],
+  counter:0
+},
+handleBtnClick(){
+  //1.错误做法：界面是不会刷新的
+  //this.data.counter +=1
+  //console.log(this.data.counter)
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  //2.this.setData()
+  this.setData({
+    counter: ++this.data.counter
+  })
+},
+handleSubtract(){
+this.setData({
+  counter: --this.data.counter
+})
+}
 })
